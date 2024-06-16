4ea6cb35d349094df6aa155266223c5d09dafa11
worldbreakerinmortal10 <u20306747@utp.edu.pe>
2024-06-15 21:37:10 -0500

    Update and rename omni_a05s.mk to twrp_a05s.mk

--- twrp_a05s.mk
+++ twrp_a05s.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a05s device
 $(call inherit-product, device/samsung/a05s/device.mk)
 
 PRODUCT_DEVICE := a05s
-PRODUCT_NAME := omni_a05s
+PRODUCT_NAME := twrp_a05s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A057M
 PRODUCT_MANUFACTURER := samsung
